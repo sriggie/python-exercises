dfe3ef3c9b04f5eab30e7f43e06b360ade2809bc
sriggie <128946324+sriggie@users.noreply.github.com>
2023-06-14 17:45:23 +0300

    velocity and variables

---
+++ program-excercises.py
@@ -0,0 +1,9 @@
+#create a python program that calculates the accelaration of an object whose initial velocity , final velocity initial time and final time is 0, 20, 0 and 10 resp.
+#  sol :
+# create 5 variables
+t1 = 0
+t2 = 10
+v1 = 0
+v2 = 20
+accelaration = (v1 - v2 ) / (t1 - t2)
+print(f"Acelaration is : {accelaration}")
