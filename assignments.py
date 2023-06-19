f77d501ac74e9cc7aadd0a9c1332c612520c5e40
sriggie <128946324+sriggie@users.noreply.github.com>
2023-06-19 17:48:18 +0300

    create a python calculator assignement

---
+++ assignments.py
@@ -0,0 +1,7 @@
+# create a a scientific calculator which displays an interactive menu to the user to allow them to pick an operation
+#  create at leats 11 operations
+# operations -> addition , subtraction , multiplication , division , modulus , power , sqrt , log , sine, cosine , tanget
+#  use while loop - > to create an interactive menu
+#  use if statements
+#  use the math module
+# use the break , continue statements 
